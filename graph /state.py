bbd61b91f925dd32d6965d3b6f7c68f71ef58d92
Harshitpandey21 <harshitpandey1234789@gmail.com>
2026-01-18 00:43:01 +0530

    first commit

---
+++ graph /state.py
@@ -0,0 +1,11 @@
+from typing import TypedDict 
+
+class CodeState(TypedDict):
+    raw_code: str
+    parsed_summary: str
+    review_code : str
+    refactored_code: str
+    test_report: str
+    human_approval: str 
+     
+
