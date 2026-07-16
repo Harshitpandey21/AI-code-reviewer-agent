d63405e56170fce1e53a56d1536fa93341c108e3
Harshit Pandey <harshitpandey1234789@gmail.com>
2026-07-16 16:23:28 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
 
 ALLOWED_ACTIONS = {
     "PROJECT_REVIEW": ("AI Project Review Report", "review_report"), # Project review 
-    "PROJECT_EXPLAIN": ("AI Project Explanation", "project_explanation"), # Project explain
+    "PROJECT_EXPLAIN": ("AI Project Explanation", "project_explanation"), # Project explanation
     "INTERVIEW": ("AI Interview Questions", "interview_questions"), # interview questions 
     "DOCUMENTATION": ("AI Project Documentation", "documentation_generation"), # Readme document generation
 }
