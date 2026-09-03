d362efa117f07ef1c52b68e9182da2db9a534001
Harshit Pandey <harshitpandey1234789@gmail.com>
2026-09-03 17:07:20 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI() # FastAPI server
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # allowed origins
+    allow_origins=["*"], # all origins are allowed
     allow_methods=["*"], # allowed methods
     allow_headers=["*"], # allowed headers 
 )
