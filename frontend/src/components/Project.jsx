d7c5b308cc74f817f721f31aee9b7329ee7cb200
Harshit Pandey <harshitpandey1234789@gmail.com>
2026-09-13 08:24:03 +0530

    Update Project.jsx

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -5,7 +5,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 const TAB_IDS = [
   "PROJECT_REVIEW", # Project Review Tab
   "PROJECT_EXPLAIN", # Project Explantion tab 
-  "INTERVIEW",
+  "INTERVIEW", # Project Interview tab
   "DOCUMENTATION",
 ];
 
