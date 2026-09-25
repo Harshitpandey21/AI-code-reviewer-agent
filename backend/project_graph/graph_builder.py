15de0312bb6e337ccef3f667a8fd8bd9d7a6a9e8
Harshit Pandey <harshitpandey1234789@gmail.com>
2026-09-25 16:21:46 +0530

    Update graph_builder.py

--- backend/project_graph/graph_builder.py
+++ backend/project_graph/graph_builder.py
@@ -35,7 +35,7 @@ graph.add_conditional_edges(
 )
 
 graph.add_edge("PROJECT_REVIEW", END)# Project review edge
-graph.add_edge("PROJECT_EXPLAIN", END)
+graph.add_edge("PROJECT_EXPLAIN", END) # Project Explain edge
 graph.add_edge("INTERVIEW", END)
 graph.add_edge("DOCUMENTATION", END)
 
